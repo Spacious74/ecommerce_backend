1cde040f924aee500d889581df0b6f127db60fdf
VishalS <vishalsavita809018@gmail.com>
2023-08-13 20:41:18 +0530

    Changes mongodb url

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const bodyParser = require('body-parser');
 const cloudinary = require('cloudinary').v2;
 const cors = require('cors');
 
-// dotev configuration for Constants
-dotenv.config({path : "./config/config.env"});
 
 // main application
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// dotev configuration for Constants
+dotenv.config({path : "./config/config.env"});
 
 // all routers
 const allRouters = require('./routes/allRoutes');
 
 // port
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 // mongodb connection
 require('./dbconnection')();
